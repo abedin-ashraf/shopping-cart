0f3cbbd5b384e933f28690bd4be3ab5a7d4d6cb5
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-06-14 10:16:27 -0500

    added features

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-dsf
+document.getElementById('case-plus').addEventListener('click', function () {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+
+    caseInput.value = parseInt(caseNumber) + 1;
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+
+    caseInput.value = parseInt(caseNumber) - 1;
+})
